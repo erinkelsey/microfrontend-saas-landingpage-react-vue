45263192cb3051e133822aaf68d955a221a0dc43
erinkelsey <erinszilagyi@gmail.com>
2020-12-01 03:09:13 -0600

    Container initial setup

---
+++ container/config/webpack.dev.js
@@ -0,0 +1,20 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const commonConfig = require("./webpack.common");
+
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
